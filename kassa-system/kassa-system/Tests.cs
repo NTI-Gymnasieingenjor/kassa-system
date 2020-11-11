16ce0f77516a9e8743c6276ac876ec60c568f488
Pontus Granath <pontus.granath@learnet.se>
2020-11-11 11:23:20 +0100

    Made test for Bulle button

---
+++ kassa-system/kassa-system/Tests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Extensions.Forms;
+using NUnit.Framework;
+
+namespace kassa_system
+{
+    [TestFixture]
+    class Tests
+    {
+        Form1 form = new Form1();
+
+        [Test]
+        public void TestBulle() 
+        {
+            form.Show();
+            ControlTester tbxSumma = new ControlTester("tbxSumma");
+            ButtonTester btnBulle = new ButtonTester("btnBulle");
+
+            Assert.AreEqual("0", tbxSumma.Text);
+
+            btnBulle.Click();
+
+            Assert.AreEqual("10", tbxSumma.Text);
+
+            Assert.AreEqual("Bulle", btnBulle.Text);
+        }
+        
+    }
+}
