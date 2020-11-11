b3a8cc42724f00b7904b2acc2369df11ef950c8e
Pontus Granath <pontus.granath@learnet.se>
2020-11-11 16:02:20 +0100

    Refactoring tests

--- kassaSystem/kassaSystem/Form1.Designer.cs
+++ kassaSystem/kassaSystem/Form1.Designer.cs
@@ -72,7 +72,7 @@
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
-            this.Text = "Kassa System";
+            this.Text = "Kassasystem";
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- kassaSystem/kassaSystem/Tests.cs
+++ kassaSystem/kassaSystem/Tests.cs
@@ -12,6 +12,10 @@ namespace kassaSystem
     [TestFixture]
     class Tests
     {
+        // Looks for the tbxSumma element and btnBulle element
+        ControlTester tbxSumma = new ControlTester("tbxSumma");
+        ButtonTester btnBulle = new ButtonTester("btnBulle");
+
         [SetUp]
         public void SetUp()
         {
@@ -23,23 +27,47 @@ namespace kassaSystem
         }
 
         [Test]
-        public void BulleButtonTest()
+        public void SummaIsZero()
         {
-
-            // Looks for the tbxSumma element and btnBulle element
-            ControlTester tbxSumma = new ControlTester("tbxSumma");
-            ButtonTester btnBulle = new ButtonTester("btnBulle");
-
             // Checks if tbxSumma text is equal to 0
             Assert.AreEqual("0", tbxSumma.Text);
+        }
 
+
+
+        [Test]
+        public void BulleButtonTest()
+        {
             // Clicks btnBulle button
             btnBulle.Click();
 
+            // Checks if tbxSumma text is equal to 10 after button click
             Assert.AreEqual("10", tbxSumma.Text);
 
-            Assert.AreEqual("Bulle", btnBulle.Text);
 
+        }
+    }
+    class CheckBulleText
+    {
+        // Looks for the tbxSumma element and btnBulle element
+        ControlTester tbxSumma = new ControlTester("tbxSumma");
+        ButtonTester btnBulle = new ButtonTester("btnBulle");
+        [SetUp]
+        public void SetUp()
+        {
+            // Defines Form
+            Form1 form = new Form1();
+
+            // Allows the form the be tested
+            form.Show();
+
+        }
+
+        [Test]
+        public void CheckBulleTxt()
+        {
+            // Checks if btnBulle text is Bulle
+            Assert.AreEqual("Bulle", btnBulle.Text);
         }
     }
 }
