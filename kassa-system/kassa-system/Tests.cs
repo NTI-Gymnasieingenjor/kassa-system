2bf76b72fc4739b5c4144eb8bccbff5e1eec9f74
Pontus Granath <pontus.granath@learnet.se>
2020-11-11 11:38:14 +0100

    Added comments

--- kassa-system/kassa-system/Tests.cs
+++ kassa-system/kassa-system/Tests.cs
@@ -11,17 +11,29 @@ namespace kassa_system
     [TestFixture]
     class Tests
     {
-        Form1 form = new Form1();
+        [SetUp]
+        public void SetUp()
+        {
+            // Defines Form
+            Form1 form = new Form1();
+
+            // Allows the form the be tested
+            form.Show();
+        }
+
+
 
         [Test]
         public void TestBulle() 
         {
-            form.Show();
+            // Looks for the tbxSumma element and btnBulle element
             ControlTester tbxSumma = new ControlTester("tbxSumma");
             ButtonTester btnBulle = new ButtonTester("btnBulle");
 
+            // Checks if tbxSumma text is equal to 0
             Assert.AreEqual("0", tbxSumma.Text);
 
+            // Clicks btnBulle button
             btnBulle.Click();
 
             Assert.AreEqual("10", tbxSumma.Text);
