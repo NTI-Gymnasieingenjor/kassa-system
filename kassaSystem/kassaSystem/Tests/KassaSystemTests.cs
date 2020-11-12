ea7a9d32ca786a1a759f422ecf4f275e73cea7ae
Pontus Granath <pontus.granath@learnet.se>
2020-11-12 11:17:53 +0100

    Made test for nollstall button

--- kassaSystem/kassaSystem/Tests/KassaSystemTests.cs
+++ kassaSystem/kassaSystem/Tests/KassaSystemTests.cs
@@ -36,7 +36,7 @@ namespace kassaSystem
 
             buttonBulle.Click();
 
-            // Checks if tbxSumma text is equal to 10 after button click
+            // Checks if textboxSumma text is equal to 10 after button click
             Assert.AreEqual("10", textboxSumma.Text);
         }
 
@@ -50,8 +50,24 @@ namespace kassaSystem
             buttonBulle.Click();
             buttonBulle.Click();
 
-            // Checks if tbxSumma text is equal to 20 after button click
+            // Checks if textboxSumma text is equal to 20 after button click
             Assert.AreEqual("20", textboxSumma.Text);
+        }
+
+        [Test]
+        public void TestIfResetButtonResetsSumma()
+        {
+            // Gets elements from form
+            ButtonTester buttonBulle = new ButtonTester("buttonBulle", "KassaSystem");
+            ButtonTester buttonNollstall = new ButtonTester("buttonNollstall", "KassaSystem"); 
+            TextBoxTester textboxSumma = new TextBoxTester("textboxSumma", "KassaSystem");
+
+            buttonBulle.Click();
+
+            buttonNollstall.Click();
+
+            // Checks if textboxSumma text is equal to 0 after button click
+            Assert.AreEqual("0", textboxSumma.Text);
         }
 
         [Test]
@@ -60,7 +76,7 @@ namespace kassaSystem
             // Gets element from form
             TextBoxTester textboxSumma = new TextBoxTester("textboxSumma", "KassaSystem");
 
-            // Checks if tbxSumma text is equal to 0
+            // Checks if textboxSumma text is equal to 0
             Assert.AreEqual("0", textboxSumma.Text);
         }
 
@@ -70,7 +86,7 @@ namespace kassaSystem
             // Gets element from form
             ButtonTester buttonBulle = new ButtonTester("buttonBulle", "KassaSystem");
 
-            // Checks if btnBulle text is Bulle
+            // Checks if buttonBulle text is Bulle
             Assert.AreEqual("Bulle", buttonBulle.Text);
         }
     }
