fcdef003ea6ad0527ef53acfa7f065ee3bfcaa33
SebastianLanden <sebastian.landen@elev.ga.ntig.se>
2020-11-12 11:25:34 +0100

    Revert "made test for nollstall button text"
    This reverts commit 225d6769f9b16db4d78c7e367a032dba63fc4b42.

--- kassaSystem/kassaSystem/Tests/KassaSystemTests.cs
+++ kassaSystem/kassaSystem/Tests/KassaSystemTests.cs
@@ -73,15 +73,5 @@ namespace kassaSystem
             // Checks if btnBulle text is Bulle
             Assert.AreEqual("Bulle", buttonBulle.Text);
         }
-
-        [Test]
-        public void TestCheckNollstallText()
-        {
-            // Gets element from form
-            ButtonTester buttonNollstall = new ButtonTester("buttonNollstall", "KassaSystem");
-
-            // Checks if buttonNollstall text is Nollställ
-            Assert.AreEqual("Nollställ", buttonNollstall.Text);
-        }
     }
 }
